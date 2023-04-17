5d5d71681a4e7c174f544c6ad96c92173abf2559
mcanet <mar.canet@gmail.com>
2023-04-17 20:38:24 +0300

    fix link for source
    fix link for source

--- src/InfoFields.js
+++ src/InfoFields.js
@@ -15,13 +15,17 @@ class InfoFields extends Component {
             infos,
         } = this.props;
 
+        const linkStyle = {
+            color: 'CornflowerBlue'
+        };
+
         const InfoList = infos.map((info) => 
             <Fragment>
                 <Grid key={uuid()} item xs={4}>
                     {info}:
                 </Grid>
                 <Grid key={uuid()} item xs={8}>   
-                    {( typeof  metadata[hover_index][info] === 'string' && metadata[hover_index][info].length>4 && metadata[hover_index][info].substring(0, 4)==='http')  ? <a href={metadata[hover_index][info]} target="_blank" rel="noreferrer">{metadata[hover_index][info]}</a>: metadata[hover_index][info]}
+                    {( typeof  metadata[hover_index][info] === 'string' && metadata[hover_index][info].length>4 && metadata[hover_index][info].substring(0, 4)==='http')  ? <a href={metadata[hover_index][info]} target="_blank" rel="noreferrer" style={linkStyle}>{metadata[hover_index][info]}</a>: metadata[hover_index][info]}
                 </Grid>
             </Fragment>
         )
